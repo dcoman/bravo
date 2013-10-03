17069d32bd8a715656f223d8767afbcd1a24345c
Dan <dcoman@pitechplus.com>
2013-10-03 19:40:03 +0300

    Registration page + activation mail.

--- src/Shop/BookshopBundle/Entity/User.php
+++ src/Shop/BookshopBundle/Entity/User.php
@@ -60,4 +60,152 @@ class User extends BaseUser
         parent::__construct();
     }
 
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string 
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string 
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param integer $phone
+     * @return User
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return integer 
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set gender
+     *
+     * @param integer $gender
+     * @return User
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    
+        return $this;
+    }
+
+    /**
+     * Get gender
+     *
+     * @return integer 
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * Set billing_address_id
+     *
+     * @param integer $billingAddressId
+     * @return User
+     */
+    public function setBillingAddressId($billingAddressId)
+    {
+        $this->billing_address_id = $billingAddressId;
+    
+        return $this;
+    }
+
+    /**
+     * Get billing_address_id
+     *
+     * @return integer 
+     */
+    public function getBillingAddressId()
+    {
+        return $this->billing_address_id;
+    }
+
+    /**
+     * Set shipping_address_id
+     *
+     * @param integer $shippingAddressId
+     * @return User
+     */
+    public function setShippingAddressId($shippingAddressId)
+    {
+        $this->shipping_address_id = $shippingAddressId;
+    
+        return $this;
+    }
+
+    /**
+     * Get shipping_address_id
+     *
+     * @return integer 
+     */
+    public function getShippingAddressId()
+    {
+        return $this->shipping_address_id;
+    }
 }
